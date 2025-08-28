48d58372b6c54001fab06bb0930c927c095210fe
AlimulDevs <alimuldev@gmail.com>
2025-08-28 11:52:59 +0700

    add 567

--- app/controller/bankController/api_get_all.go
+++ app/controller/bankController/api_get_all.go
@@ -2,31 +2,34 @@ package bankcontroller
 
 import (
 	"api/app/lib"
+	"api/app/models/dto/bankDto"
+	"api/app/models/model/bankModel"
 
 	"api/app/services"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/copier"
 )
 
 func GetBank(c *fiber.Ctx) error {
-	services.DB.WithContext(c.UserContext())
+	db := services.DB.WithContext(c.UserContext())
 
-	// var model []bankmodel.BankModel
+	var model []bankModel.BankModel
 
-	// var dto []bankdto.BankResponse
+	var dto []bankDto.BankResponse
 
-	// err := db.Find(&model).Error
+	err := db.Find(&model).Error
 
-	// if err != nil {
-	// 	return lib.ErrorNotFound(c)
-	// }
-	// copier.Copy(&dto, &model)
+	if err != nil {
+		return lib.ErrorNotFound(c)
+	}
+	copier.Copy(&dto, &model)
 
 	response := lib.BaseResponse{
 		Status:  "success",
 		Code:    200,
 		Message: "OK",
-		Data:    nil,
+		Data:    dto,
 	}
 
 	return lib.OK(c, response)
