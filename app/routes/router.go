465eab353df06a858590bd38e057fbddf404674a
AlimulDevs <alimuldev@gmail.com>
2025-09-06 11:36:25 +0700

    aadd 2

--- app/routes/router.go
+++ app/routes/router.go
@@ -37,31 +37,31 @@ func Handle(app *fiber.App) {
 	api.Get("/", controller.GetAPIIndex)
 	api.Get("/info.json", controller.GetAPIInfo)
 
-	api.Post("branch/get-all", middleware.TokenValidator(), branchController.PostBranchGetAll)
-	api.Get("branch", middleware.TokenValidator(), branchController.GetBranchParent)
-	api.Get("branch/children", middleware.TokenValidator(), branchController.GetBranchChildren)
-	api.Get("branch/children/parent/:parent_code", middleware.TokenValidator(), branchController.GetBranchChildrenByParent)
-	api.Get("branch/:branch_code/detail", middleware.TokenValidator(), branchController.GetBranchDetailByCode)
+	api.Post("branch/get-all", middleware.VerifyTokenJwt(), branchController.PostBranchGetAll)
+	api.Get("branch", middleware.VerifyTokenJwt(), branchController.GetBranchParent)
+	api.Get("branch/children", middleware.VerifyTokenJwt(), branchController.GetBranchChildren)
+	api.Get("branch/children/parent/:parent_code", middleware.VerifyTokenJwt(), branchController.GetBranchChildrenByParent)
+	api.Get("branch/:branch_code/detail", middleware.VerifyTokenJwt(), branchController.GetBranchDetailByCode)
 
-	api.Get("bank", middleware.TokenValidator(), bankcontroller.GetBank)
-	api.Get("bank/:id", middleware.TokenValidator(), bankcontroller.GetBankById)
+	api.Get("bank", middleware.VerifyTokenJwt(), bankcontroller.GetBank)
+	api.Get("bank/:id", middleware.VerifyTokenJwt(), bankcontroller.GetBankById)
 
-	api.Get("department", middleware.TokenValidator(), departmentController.GetDepartment)
-	api.Get("department/:code", middleware.TokenValidator(), departmentController.GetDepartmentByCode)
+	api.Get("department", middleware.VerifyTokenJwt(), departmentController.GetDepartment)
+	api.Get("department/:code", middleware.VerifyTokenJwt(), departmentController.GetDepartmentByCode)
 
-	api.Get("job-title", middleware.TokenValidator(), jobTitleController.GetJobTitle)
-	api.Get("job-title/:id", middleware.TokenValidator(), jobTitleController.GetJobTitleById)
-	api.Get("job-title/department/:department_id", middleware.TokenValidator(), jobTitleController.GetJobTitleByDepartment)
+	api.Get("job-title", middleware.VerifyTokenJwt(), jobTitleController.GetJobTitle)
+	api.Get("job-title/:id", middleware.VerifyTokenJwt(), jobTitleController.GetJobTitleById)
+	api.Get("job-title/department/:department_id", middleware.VerifyTokenJwt(), jobTitleController.GetJobTitleByDepartment)
 
-	api.Get("work-hour", middleware.TokenValidator(), workHourController.GetWorkHour)
-	api.Get("work-hour/:code", middleware.TokenValidator(), workHourController.GetWorkHourByCode)
+	api.Get("work-hour", middleware.VerifyTokenJwt(), workHourController.GetWorkHour)
+	api.Get("work-hour/:code", middleware.VerifyTokenJwt(), workHourController.GetWorkHourByCode)
 
-	api.Get("employee", middleware.TokenValidator(), employeeController.GetEmployee)
-	api.Get("employee/:id", middleware.TokenValidator(), employeeController.GetEmployeeById)
+	api.Get("employee", middleware.VerifyTokenJwt(), employeeController.GetEmployee)
+	api.Get("employee/:id", middleware.VerifyTokenJwt(), employeeController.GetEmployeeById)
 	api.Get("employee/employee-signature/:id", middleware.VerifyTokenJwt(), employeeController.GetEmployeeSignatureById)
 	api.Get("employee/employee-fcm-token/:id", middleware.VerifyTokenJwt(), employeeController.GetEmployeeFcmTokenById)
 	api.Get("employee/employee-document/:id", middleware.VerifyTokenJwt(), employeeController.GetEmployeeDocumentById)
 
-	api.Post("employee/employee-notification/get-by-employee-id", middleware.TokenValidator(), employeeNotifcationController.GetByEmployeeId)
+	api.Post("employee/employee-notification/get-by-employee-id", middleware.VerifyTokenJwt(), employeeNotifcationController.GetByEmployeeId)
 
 }
